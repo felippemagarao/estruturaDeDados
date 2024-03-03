034fba5b528a96e44d524b5570afe967b9770487
Felippe Magarão <126483942+felippemagarao@users.noreply.github.com>
2024-03-03 18:33:01 -0300

    Create retangulo.go

---
+++ ac02/geometria/retangulo.go
@@ -0,0 +1,9 @@
+package geometria
+
+func AreaRetangulo(largura, altura, float64) float64 {
+	return largura*altura
+}
+
+func PerimetroRetangulo(largura, altura, float64) float64{
+	return 2*(largura+altura)
+}
